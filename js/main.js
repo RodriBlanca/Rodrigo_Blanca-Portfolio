679d3af8c78e892b07534ca7ebe2d9b6219a1555
RodriBlanca <rodrigoblanca96@gmail.com>
2021-09-11 19:46:52 -0300

    Se agregó la sección certificados

--- js/main.js
+++ js/main.js
@@ -7,16 +7,14 @@ class SkillDescription {
 }
 
 // VARIABLES
-
 const navMenu = document.querySelector('.nav-menu');
 const heroIntroduction = document.querySelector('.hero-introduction');
 const aboutMe = document.querySelector('.about-me');
 const skills = document.querySelector('.skills');
 const projects = document.querySelector('.projects');
-const contact = document.querySelector('.contact');
+const certificates = document.querySelector('.certificates');
 const upArrowBtn = document.querySelector('.arrow');
 const header = document.querySelector('.header');
-const descriptionContainer = document.querySelector('.skills-description');
 const skillsItems = document.querySelectorAll('.skills-list__item');
 const emailInput = document.querySelector('.email');
 const subjectInput = document.querySelector('.subject');
@@ -26,6 +24,16 @@ const linkedin = document.querySelector('.social-linkedin');
 const github = document.querySelector('.social-github');
 const menuButton = document.querySelector('.mobile-menu');
 const mobileMenu = document.querySelector('.mobile-options');
+const htmlItem = document.getElementById('html-item');
+const cssItem = document.getElementById('css-item');
+const jsItem = document.getElementById('js-item');
+const sassItem = document.getElementById('sass-item');
+const bootstrapItem = document.getElementById('bootstrap-item');
+const gitItem = document.getElementById('git-item');
+const englishItem = document.getElementById('english-item');
+const skillsContainer = document.getElementById('skills-description');
+const aboutMobileButton = document.getElementById('about-menu');
+const mobileMenu = document.getElementById('mobile-menu');
 
 window.addEventListener('load', () => {
     if(!heroIntroduction.style.opacity) {
@@ -54,7 +62,6 @@ window.addEventListener('scroll', () => {
 /* PROJECTS SECTION EFFECT */
 window.addEventListener('scroll', () => {
     const projectsUbication = projects.getBoundingClientRect();
-    console.log(projectsUbication);
     if(projectsUbication.top < 467) {
         projects.style.opacity = 1;
         projects.classList.add('projects-show');
@@ -89,7 +96,8 @@ window.addEventListener('scroll', () => {
     const aboutMeUbication = aboutMe.getBoundingClientRect();
     const skillsUbication = skills.getBoundingClientRect();
     const projectsUbication = projects.getBoundingClientRect();
-    const contactUbication = contact.getBoundingClientRect();
+    const certificatesUbication = certificates.getBoundingClientRect();
+    console.log(certificatesUbication);
 
     /* ABOUT */
     if(aboutMeUbication.top < 446 && aboutMeUbication.top > -230) {
@@ -101,7 +109,7 @@ window.addEventListener('scroll', () => {
     }
 
     /* SKILLS */
-    if(skillsUbication.top < 200 && skillsUbication.top > -362) {
+    if(projectsUbication.top < 200 && projectsUbication.top > -362) {
         navMenu.children[2].children[0].style.color = '#e30613';
         navMenu.children[2].children[0].style.textShadow = '2.3px 2.3px 3px rgb(136, 136, 136)';
     } else {
@@ -110,7 +118,7 @@ window.addEventListener('scroll', () => {
     }
 
     /* PROJECTS */
-    if(projectsUbication.top < 185 && projectsUbication.top > -352) {
+    if(skillsUbication.top < 185 && skillsUbication.top > -352) {
         navMenu.children[4].children[0].style.color = '#e30613';
         navMenu.children[4].children[0].style.textShadow = '2.3px 2.3px 3px rgb(136, 136, 136)';
     } else {
@@ -118,8 +126,8 @@ window.addEventListener('scroll', () => {
         navMenu.children[4].children[0].style.textShadow = '0px 0px 0px transparent';
     }
 
-    /* CONTACT */
-    if(contactUbication.top < 223 && contactUbication.top > -179) {
+    /* CERTIFICATES */
+    if(certificatesUbication.top < 223 && certificatesUbication.top > -830) {
         navMenu.children[6].children[0].style.color = '#e30613';
         navMenu.children[6].children[0].style.textShadow = '2.3px 2.3px 3px rgb(136, 136, 136)';
     } else {
@@ -214,14 +222,14 @@ menuButton.addEventListener('click', () => {
 })
 
 /* TO SECTION */
-contact.addEventListener('click', () => {
+// contact.addEventListener('click', () => {
     // window.scrollTo({
     //     top: 2520,
     //     left: 0,
     //     behavior: "smooth"
     // })
-    console.log('f')
-})
+    // console.log('f')
+// })
 
 // window.addEventListener('scroll', () => {
 //     console.log(window.scrollY)
@@ -265,9 +273,6 @@ upArrowBtn.addEventListener('click', (e) => {
 
 
 /* SKILLS DESCRIPTIONS */
-// console.log(skillsItems.length);
-console.log(skillsItems[0].children[1]);
-
 for(let i = 0; i < skillsItems.length; i++) {
     skillsItems[i].addEventListener('click', (e) => {
         console.log(e.target);
@@ -283,54 +288,133 @@ for(let i = 0; i < skillsItems.length; i++) {
 function createDescription(i) {
     const skillTiltle = skillsItems[i].children[1].textContent;
     const descriptionItem = new SkillDescription(skillTiltle);
-    console.log(descriptionItem)
+    descriptionHTML(skillTiltle);
     return descriptionItem;
 }
 
 /* CREATES HTML DESCRIPTION */
-// function descriptionHTML() {
-//     switch(skillsItems[i].children[1].textContent) {
-//         case 'HTML': 
+function descriptionHTML(skill) {
+    clearDescription();
+    switch(skill) {
+        case 'HTML': 
+            htmlItem.style.color = '#e30613';
+            const htmlDescription = document.createElement('div');
+            htmlDescription.classList.add('description-effect');
+            htmlDescription.classList.add('description-item');
+            htmlDescription.innerHTML = `
+                <p class="skills-description__item">Empecé a estudiar HTML de manera autodidácta mediante cursos de YouTube a finales del 2020.<br>
+                Dichos conocimientos me fueron útiles para obtener luego el certificado de Responsive Web Design de freeCodeCamp y lograr un 10 en el proyecto final del curso de Desarrollo Web de Coderhouse, institución a la que me inscribiría luego para ampliar mis conocimientos.</p>
+            `;
+            skillsContainer.appendChild(htmlDescription);
+            break;
+        case 'CSS':
+            cssItem.style.color = '#e30613';
+            const cssDescription = document.createElement('div');
+            cssDescription.classList.add('description-effect');
+            cssDescription.classList.add('description-item');
+            cssDescription.innerHTML = `
+                <p class="skills-description__item">De igual forma que con HTML, comencé a estudiar CSS de manera autodidácta. Dichos conocimientos me fueron útiles para obtener el certificado en Desarrollo Web en Coderhouse.<br>
+                Al ser el diseño una de las cosas que más me apasionan del desarrollo front-end, sigo aprendiendo mediante cursos en Udemy nuevas técnicas para mejorar el apartado visual de mis sitios web.<br>
+                Cuento con conocimientos en box-modeling, pseudo-clases, media queries, flexbox, grid y animaciónes.</p>
+            `;
+            skillsContainer.appendChild(cssDescription);
+            break;
+        case 'JAVASCRIPT':
+            jsItem.style.color = '#e30613';
+            const jsDescription = document.createElement('div');
+            jsDescription.classList.add('description-effect');
+            jsDescription.classList.add('description-item');
+            jsDescription.innerHTML = `
+                <p class="skills-description__item">Finalicé el curso de JavaScript en Coderhouse obteniendo la certificación del curso en Agósto del 2021.<br>
+                Además, en simultáneo a la cursada, estúve realizando el curso de "JavaScript Moderno" en Udemy para seguir perfeccionándome en el lenguaje y aprender cosas que por los tiempos de la cursada a veces se hace difícil.<br>
+            Encontré en JavaScript un lenguaje que permite darle mucha vida a las páginas web.<br>
+            Cuento con los siguientes conocimientos: variables, iteradores, condicionales, funciones tradicionales y funciones de flecha, arreglos, objetos,POO, clases, desestructuración de objetos y arreglos, DOM, eventos, storage, AJAX y jQuery. 
+            </p>
+            `;
+            skillsContainer.appendChild(jsDescription);
+            break;
+        case 'SASS':
+            sassItem.style.color = '#e30613';
+            const sassDescription = document.createElement('div');
+            sassDescription.classList.add('description-effect');
+            sassDescription.classList.add('description-item');
+            sassDescription.innerHTML = `
+                <p class="skills-description__item">Aprendí Sass en el curso de Desarrollador Web de Coderhouse y es una herramienta que sigo utilizando a día de hoy en todos mis proyectos.</p>
+            `;
+            skillsContainer.appendChild(sassDescription);
+            break;
+        case 'BOOTSTRAP':
+            bootstrapItem.style.color = '#e30613';
+            const bootstrapDescription = document.createElement('div');
+            bootstrapDescription.classList.add('description-effect');
+            bootstrapDescription.classList.add('description-item');
+            bootstrapDescription.innerHTML = `
+                <p class="skills-description__item">Bootstrap es otra de las herramientas que aprendí en el primer curso de Coderhouse. Considero que tengo un nivel básico, ya que por gusto personal, de preferir hacer las cosas por mi cuenta; y por ciertas limitaciones que tenía con Bootstrap, decidí no aplicarlo mucho en el proyecto.</p>
+            `;
+            skillsContainer.appendChild(bootstrapDescription);
+            break;
+        case 'GIT':
+            gitItem.style.color = '#e30613'; 
+            break;  
+        case 'ENGLISH':
+            englishItem.style.color = '#e30613';
+            break;
+    }
+}
 
-//         case 
-//     }
-// }
+/* VERIFICA SI HAY OTRO SKILL ITEM EN EL CONTENEDOR DE LA DESCRIPCIÓN */
+function clearDescription() {
+    /* LIMPIA EL CONTENEDOR */
+    if(skillsContainer.children.length > 0) {
+        skillsContainer.textContent = '';
+    }
 
-
+    /* QUITA EL ESTILO A LOS ITEMS SELECCIONADOS ANTERIORMENTE */
+    skillsItems.forEach(skill => {
+        if(skill.style.color == 'rgb(227, 6, 19)') {
+            skill.style.color = '#131928';
+        }
+    })
+}
 
 /* CONTACT */
-emailInput.addEventListener('blur', (e) => {
-    e.preventDefault();
-    if(e.target.value.length === 0) {
-        console.log('Todos los campos son obligatorios');
-        emailInput.classList.add('error');
-    } else {
-        if(e.target.value.includes('@')) {
-            console.log(e.target.value);
-            emailInput.classList.add('validate');
-        } else {
-            console.log('El dato ingresado no es un mail');
-            emailInput.classList.add('error');
-        }
-    }
-})
+// emailInput.addEventListener('blur', (e) => {
+//     e.preventDefault();
+//     if(e.target.value.length === 0) {
+//         console.log('Todos los campos son obligatorios');
+//         emailInput.classList.add('error');
+//     } else {
+//         if(e.target.value.includes('@')) {
+//             console.log(e.target.value);
+//             emailInput.classList.add('validate');
+//         } else {
+//             console.log('El dato ingresado no es un mail');
+//             emailInput.classList.add('error');
+//         }
+//     }
+// })
 
-subjectInput.addEventListener('blur', (e) => {
-    e.preventDefault();
-    if(e.target.value.length === 0) {
-        console.log('Todos los campos son obligatorios');
-        subjectInput.classList.add('error');
-    } else {
-        console.log(e.target.value);
-        subjectInput.classList.add('validate');
-    }
-})
+// subjectInput.addEventListener('blur', (e) => {
+//     e.preventDefault();
+//     if(e.target.value.length === 0) {
+//         console.log('Todos los campos son obligatorios');
+//         subjectInput.classList.add('error');
+//     } else {
+//         console.log(e.target.value);
+//         subjectInput.classList.add('validate');
+//     }
+// })
 
-messageInput.addEventListener('blur', (e) => {
-    e.preventDefault();
-    if(e.target.value.length === 0) {
-        console.log('Todos los campos son obligatorios');
-    } else {
-        console.log(e.target.value);
-    }
+// messageInput.addEventListener('blur', (e) => {
+//     e.preventDefault();
+//     if(e.target.value.length === 0) {
+//         console.log('Todos los campos son obligatorios');
+//     } else {
+//         console.log(e.target.value);
+//     }
+// })
+
+/* MOBILE MENÚ */
+aboutMobileButton.addEventListener('click', () => {
+    console.log('funciona');
 })
